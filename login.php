e663799511a925773a38b488b18803d3cd19d0ec
Zikry Danial <zikrydanial@graduate.utm.my>
2024-06-22 11:26:50 +0800

    x

---
+++ login.php
@@ -0,0 +1,32 @@
+<?php
+session_start();
+
+$servername = "your_server";
+$username = "your_username";
+$password = "your_password";
+$dbname = "your_database";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $user = $conn->real_escape_string($_POST['username']);
+    $pass = $conn->real_escape_string($_POST['password']);
+
+    $sql = "SELECT * FROM users WHERE username='$user' AND password='$pass'";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        $_SESSION['loggedin'] = true;
+        $_SESSION['username'] = $user;
+        header("Location: index.html");
+    } else {
+        echo "Invalid username or password";
+    }
+}
+
+$conn->close();
+?>
